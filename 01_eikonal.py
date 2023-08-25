07be635a7092ae549706cf4ecedbc2b7b12c54d1
Samuel Burbulla <s.burbulla@appliedai-institute.de>
2023-08-25 16:29:19 +0200

    Fix eikonal.

--- 01_eikonal.py
+++ 01_eikonal.py
@@ -5,36 +5,50 @@ from tpinn.geometry import Transformed
 import deepxde as dde
 import matplotlib.pyplot as plt
 
-dim = 1
-ref = dde.geometry.Rectangle([0]*dim, [1]*dim)
+l = 3 / 2 * torch.pi
+ref = dde.geometry.Rectangle([0], [l])
 
-to_global = lambda x: 10 * x
-to_local = lambda x: x / 10
+def to_global(x):
+    return torch.stack([
+        torch.sin(x),
+        torch.cos(x),
+        ]).transpose(0, -1).squeeze(0)
 
-geom = Transformed(ref, to_global, to_local)
+dim = 2
 
-# Eikonal equation with respect to global coordinates
+# Eikonal equation: grad(u) = 1
 def pde(y, u):
     grad = 0
-    for j in range(geom.dim):
-        grad += dde.grad.jacobian(u, y, i=0, j=j)**2
-    return grad - 1
+    for j in range(ref.dim):
+        grad += dde.grad.jacobian(u, y, i=0, j=j)
+    return (grad - 1)**2
 
-data = dde.data.PDE(geom, pde, [], num_domain=100**geom.dim)
-net = dde.nn.FNN([geom.dim] + [128] * 3 + [1], "tanh", "Glorot uniform")
+data = dde.data.PDE(ref, pde, [], num_domain=100)
+net = dde.nn.FNN([dim] + [128] * 3 + [1], "tanh", "Glorot uniform")
+net.apply_feature_transform(lambda x: to_global(x))
 
-# Impose zero Dirichlet boundary condition and positive solution
-net.apply_output_transform(lambda x, u: geom.distance2boundary(x) * u**2)
+# Zero Dirichlet boundary condition u(0) = 0
+net.apply_output_transform(lambda x, u: x * u)
 
 model = dde.Model(data, net)
-model.compile("adam", lr=1e-4)
+dde.optimizers.config.set_LBFGS_options(maxiter=100)
+model.compile("L-BFGS")
+model.train()
 
-# Solve with transformation to local coordinates
-net.apply_feature_transform(lambda x: geom.to_local(x))
-model.train(epochs=15000)
+# Evaluate solution
+x = ref.uniform_points(1000)
+x = torch.tensor(x)
+u = net(x)
+u = u.detach().numpy()
 
-# Plot solution
-x = geom.uniform_points(1000)
-u = net(torch.tensor(x)).detach().numpy()
-plt.plot(x[:, 0], u, 'b-')
+# Plot in global coordinates
+x = to_global(x)
+plt.scatter(x[:, 0], x[:, 1], s=10, c=u)
+plt.colorbar()
+plt.axis('equal')
 plt.savefig("01_eikonal.png")
+
+# Check that the solution is correct
+umax = u.max()
+print(f"u_max: {umax:.3f}  expected: {l:.3f}\n")
+assert abs(umax - l) < 1e-2
