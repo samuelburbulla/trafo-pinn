a3bfc6dd4e02c5d0939a8f3478a4393aeb0989c1
Samuel Burbulla <s.burbulla@appliedai-institute.de>
2023-08-31 12:12:00 +0200

    Improve 02.

--- 02_surface.py
+++ 02_surface.py
@@ -1,40 +1,47 @@
 import torch
 torch.manual_seed(0)
 
-from tpinn.geometry import Transformed
+import tpinn.geometry
+import tpinn.grad
 import deepxde as dde
 import matplotlib.pyplot as plt
 
 ref = dde.geometry.Rectangle([0, 0], [1, 1])
 
+# Spherical coordinates
+phi0, theta0 = 0.25, 1.0
+
 def to_global(x):
-    phi0, theta0 = .25, -1
-    return torch.stack([
-        torch.sin(x[:, 0:1] + phi0) * torch.cos(x[:, 1:2] + theta0),
-        torch.sin(x[:, 0:1] + phi0) * torch.sin(x[:, 1:2] + theta0),
-        torch.cos(x[:, 0:1] + phi0),
-    ]).transpose(0, -1).squeeze(0)
+    phi = x[:, 0:1] + phi0
+    theta = x[:, 1:2] - theta0
+    return torch.cat((
+        x,
+        torch.sin(phi) * torch.cos(theta),
+        torch.sin(phi) * torch.sin(theta),
+        torch.cos(phi),
+    ), dim=1)
 
-dim = 3
+# Transformed geometry
+geom = tpinn.geometry.Transformed(ref, to_global)
 
-# Eikonal equation: grad(u) = 1
+# Poisson equation: -Delta(u) = 1
 def pde(y, u):
     hess = 0
     for j in range(ref.dim):
-        hess += dde.grad.hessian(u, y, i=j, j=j)
+        hess += tpinn.grad.hessian(u, y, geom, i=j, j=j)
     return (-hess - 1)**2
 
-def bc(x, u):
-    u *= x[:, 0:1] * (1 - x[:, 0:1])
-    u *= x[:, 1:2] * (1 - x[:, 1:2])
-    return u
-
-data = dde.data.PDE(ref, pde, [], num_domain=100)
-net = dde.nn.FNN([dim] + [128] * 3 + [1], "tanh", "Glorot uniform")
-net.apply_feature_transform(lambda x: to_global(x))
-
 # Zero Dirichlet boundary condition
-net.apply_output_transform(lambda x, u: bc(x, u))
+def boundary(_, on_boundary):
+    return on_boundary
+
+def zero(_):
+    return 0
+
+bc = dde.icbc.DirichletBC(geom, zero, boundary)
+data = dde.data.PDE(geom, pde, bc, num_domain=1000, num_boundary=40)
+net = dde.nn.FNN([geom.dim] + [1024] * 1 + [1], "tanh", "Glorot uniform")
+net.apply_feature_transform(lambda x: x[:, -geom.dim:])
 
 model = dde.Model(data, net)
 dde.optimizers.config.set_LBFGS_options(maxiter=100)
@@ -42,16 +49,16 @@ model.compile("L-BFGS")
 model.train()
 
 # Evaluate solution
-x = ref.uniform_points(10000)
+x = geom.uniform_points(100**geom.dim)
 x = torch.tensor(x)
 u = net(x)
 u = u.detach().numpy()
 
 # Plot in global coordinates
-x = to_global(x)
+y = x[:, -geom.dim:]
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-cb = ax.scatter(x[:, 0], x[:, 1], x[:, 2], s=10, c=u)
+cb = ax.scatter(y[:, 0], y[:, 1], y[:, 2], s=10, c=u)
 plt.colorbar(cb)
 plt.axis('equal')
 plt.savefig("02_surface.png")
