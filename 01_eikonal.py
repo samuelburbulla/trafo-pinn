9b1681978983fd547ff257afb65f7e6e1c9f08bd
Samuel Burbulla <s.burbulla@appliedai-institute.de>
2023-09-02 21:49:29 +0200

    Fix chain rule.

--- 01_eikonal.py
+++ 01_eikonal.py
@@ -2,47 +2,42 @@ import math
 import torch
 torch.manual_seed(0)
 
-import tpinn.geometry
-import tpinn.grad
 import deepxde as dde
 import matplotlib.pyplot as plt
 
-ref = dde.geometry.Interval(0, 1)
 
 # Archimedean spiral
 l = 3.5 * torch.pi
 a = 0.1
 r = lambda x: a * x
 
+ref = dde.geometry.Interval(0, 1)
+dim = 2
+
 def to_global(x):
     return torch.cat((
-        x,
         r(l * x) * torch.sin(l * x),
         r(l * x) * torch.cos(l * x),
     ), dim=1)
 
-
-geom = tpinn.geometry.Transformed(ref, to_global)
-dim = geom.dim
-rdim = ref.dim
-
-
 # Eikonal equation: grad(u) = 1
-def pde(y, u):
-    grad = tpinn.grad.jacobian(u, y, geom)
+def pde(x, u):
+    arc_length = torch.sqrt(sum(dde.grad.jacobian(to_global(x), x, i=i)**2 for i in range(dim)))
+
+    grad = dde.grad.jacobian(u, x) / arc_length
     return (grad - 1)**2
 
 # Zero Dirichlet boundary condition: u(0) = 0
-def boundary(x, on_boundary):
+def boundary(x, _):
     return dde.utils.isclose(x[0], 0)
 
 def zero(_):
     return 0
 
-bc = dde.icbc.DirichletBC(geom, zero, boundary)
-data = dde.data.PDE(geom, pde, bc, num_domain=100, num_boundary=2)
-net = dde.nn.FNN([dim] + [128] * 5 + [1], "tanh", "Glorot uniform")
-net.apply_feature_transform(lambda x: x[:, rdim:])
+bc = dde.icbc.DirichletBC(ref, zero, boundary)
+data = dde.data.PDE(ref, pde, bc, num_domain=100, num_boundary=2)
+net = dde.nn.FNN([dim] + [128] * 3 + [1], "tanh", "Glorot uniform")
+net.apply_feature_transform(lambda x: to_global(x))
 
 model = dde.Model(data, net)
 dde.optimizers.config.set_LBFGS_options(maxiter=100)
@@ -50,18 +45,19 @@ model.compile("L-BFGS")
 model.train()
 
 # Evaluate solution
-y = geom.uniform_points(1000)
-y = torch.tensor(y)
-u = net(y)
+x = ref.uniform_points(1000)
+x = torch.tensor(x)
+u = net(x)
 u = u.detach().numpy()
 
 # Plot in global coordinates
-plt.scatter(y[:, -2], y[:, -1], s=10, c=u)
+y = to_global(x)
+plt.scatter(y[:, 0], y[:, 1], s=10, c=u)
 plt.colorbar()
 plt.axis('equal')
 plt.savefig("01_eikonal.png")
 
-# Check that the solution is correct
+# Check that solution is correct
 umax = u.max()
 exact = a / 2 * (l * (1 + l**2)**.5 + math.log(l + (1 + l**2)**.5))
 print(f"u_max: {umax:.3f}  exact: {exact:.3f}\n")
--- 02_surface.py
+++ 02_surface.py
@@ -1,12 +1,11 @@
 import torch
 torch.manual_seed(0)
 
-import tpinn.geometry
-import tpinn.grad
 import deepxde as dde
 import matplotlib.pyplot as plt
 
 ref = dde.geometry.Rectangle([0, 0], [1, 1])
+dim = 3
 
 # Spherical coordinates
 phi0, theta0 = 0.25, 1.0
@@ -15,33 +14,28 @@ def to_global(x):
     phi = x[:, 0:1] + phi0
     theta = x[:, 1:2] - theta0
     return torch.cat((
-        x,
         torch.sin(phi) * torch.cos(theta),
         torch.sin(phi) * torch.sin(theta),
         torch.cos(phi),
     ), dim=1)
 
-# Transformed geometry
-geom = tpinn.geometry.Transformed(ref, to_global)
-
 # Poisson equation: -Delta(u) = 1
-def pde(y, u):
+def pde(x, u):
+    arc_length = torch.sqrt(sum(dde.grad.jacobian(to_global(x), x, i=i)**2 for i in range(dim)))
+
     hess = 0
     for j in range(ref.dim):
-        hess += tpinn.grad.hessian(u, y, geom, i=j, j=j)
+        grad = dde.grad.jacobian(u, x, j=j) / arc_length[:, j:j+1]
+        hess += dde.grad.jacobian(grad, x, j=j) / arc_length[:, j:j+1]
     return (-hess - 1)**2
 
-# Zero Dirichlet boundary condition
-def boundary(_, on_boundary):
-    return on_boundary
+data = dde.data.PDE(ref, pde, [], num_domain=100)
+net = dde.nn.FNN([dim] + [32] + [1], "tanh", "Glorot uniform")
+net.apply_feature_transform(lambda x: to_global(x))
 
-def zero(_):
-    return 0
-
-bc = dde.icbc.DirichletBC(geom, zero, boundary)
-data = dde.data.PDE(geom, pde, bc, num_domain=1000, num_boundary=40)
-net = dde.nn.FNN([geom.dim] + [1024] * 1 + [1], "tanh", "Glorot uniform")
-net.apply_feature_transform(lambda x: x[:, -geom.dim:])
+# Zero Dirichlet BC
+q = lambda z: 4 * z * (1 - z)
+net.apply_output_transform(lambda x, u: q(x[:, 0:1]) * q(x[:, 1:2]) * u)
 
 model = dde.Model(data, net)
 dde.optimizers.config.set_LBFGS_options(maxiter=100)
@@ -49,13 +43,13 @@ model.compile("L-BFGS")
 model.train()
 
 # Evaluate solution
-x = geom.uniform_points(100**geom.dim)
+x = ref.uniform_points(100**dim)
 x = torch.tensor(x)
 u = net(x)
 u = u.detach().numpy()
 
 # Plot in global coordinates
-y = x[:, -geom.dim:]
+y = to_global(x)
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 cb = ax.scatter(y[:, 0], y[:, 1], y[:, 2], s=10, c=u)
